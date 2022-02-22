e5d23ad02720f1293b29df558f60083e1626f084
Maciej Gluszek <thelameday@gmail.com>
2022-02-22 13:25:49 +0100

    little test cleanup

--- src/Components/Card/card.test.js
+++ src/Components/Card/card.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Card from './Card';
 const movie = {
   id: '1and3011',
@@ -10,26 +10,26 @@ const movie = {
 };
 
 test('Should render card', () => {
-  render(<Card movie={movie} />);
-  const cardElement = screen.getByTestId('card-1and3011');
-  expect(cardElement).toBeInTheDocument();
+  render(<Card movie={movie} />)
+  const cardElement = screen.getByTestId('card-1and3011')
+  expect(cardElement).toBeInTheDocument()
 });
 
 test('Card should display summary', () => {
-  render(<Card movie={movie} />);
-  const cardText = screen.getByTestId('card-1and3011-text');
-  expect(cardText.textContent).toBe('Lorem ipsum….');
+  render(<Card movie={movie} />)
+  const cardText = screen.getByTestId('card-1and3011-text')
+  expect(cardText.textContent).toBe('Lorem ipsum….')
 });
 
 test('Card should display title', () => {
-  render(<Card movie={movie} />);
-  const cardText = screen.getByTestId('card-1and3011-title');
-  expect(cardText.textContent).toBe('Inferno - 5.3/10');
+  render(<Card movie={movie} />)
+  const cardText = screen.getByTestId('card-1and3011-title')
+  expect(cardText.textContent).toBe('Inferno - 5.3/10')
 });
 
 test('Card image must have src and alt "bg-pic"', () => {
-  render(<Card movie={movie} />);
-  const cardText = screen.getByTestId('card-1and3011-image');
+  render(<Card movie={movie} />)
+  const cardText = screen.getByTestId('card-1and3011-image')
   expect(cardText).toHaveAttribute('src', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg' )
   expect(cardText).toHaveAttribute('alt', 'bg-pic')
 });
--- src/Components/RoundButton/roundButton.test.js
+++ src/Components/RoundButton/roundButton.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import RoundButton from './RoundButton';
 
 test('Should render button', () => {
@@ -6,4 +6,3 @@ test('Should render button', () => {
   const roundButtonElement = screen.getByTestId('round-button');
   expect(roundButtonElement).toBeInTheDocument();
 });
-
